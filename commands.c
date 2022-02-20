7fd1ea00e7126f0aec4b4b380108ed0f12e74e40
Brian Kim <hk2473@nyu.edu>
2022-02-20 17:32:59 +0400

    add fork process for commands

--- commands.c
+++ commands.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "functions.h"
 
 // To-Do: commands all require a fork b/c exec exits completely upon successs
 
 void lsCommand(void)
 {
-    execl("/bin/ls", "ls", 0, NULL);
+    pid_t child = fork();
+    if (child < 0){
+        perror("Failed fork child");
+        exit(EXIT_FAILURE);
+    }
 
+    else { //fork child succeeded
+        if (child == 0) { //currently child
+            execl("/bin/ls", "ls", 0, NULL); //exits
+        }
+        else {
+            wait(NULL);
+        }
+    }
     return;
 }
 
 void pwdCommand(void)
 {
-    execl("/bin/pwd", "pwd", 0, NULL);
+    pid_t child = fork();
+    if (child < 0){
+        perror("Failed fork child");
+        exit(EXIT_FAILURE);
+    }
 
+    else { //fork child succeeded
+        if (child == 0) { //currently child
+            execl("/bin/pwd", "pwd", 0, NULL);
+        }
+        else {
+            wait(NULL);
+        }
+    }
     return;
 }
