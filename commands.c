e240e21cde78e84376abfc14b33213892dd20caa
Brian Kim <hk2473@nyu.edu>
2022-02-23 12:14:54 +0400

    move the fork process from commands.c to mainShell.c

--- commands.c
+++ commands.c
@@ -1,61 +1,19 @@
 #include "functions.h"
 
 void lsCommand(){
-    pid_t child = fork();
-    if (child < 0){
-        perror("Failed fork child");
-        exit(EXIT_FAILURE);
-    }
-
-    else { //fork child succeeded
-        if (child == 0) { //currently child
-            execl("/bin/ls", "ls", 0, NULL);
-            perror("lsCommand"); //if we reached here, there is an error so we must exit
-            exit(EXIT_FAILURE);
-        }
-        else {
-            wait(NULL);
-        }
-    }
-    return;
+    execl("/bin/ls", "ls", 0, NULL);
+    perror("lsCommand"); //if we reached here, there is an error so we must exit
+    exit(EXIT_FAILURE);
 }
 
 void pwdCommand(){
-    pid_t child = fork();
-    if (child < 0){
-        perror("Failed fork child");
-        exit(EXIT_FAILURE);
-    }
-
-    else { //fork child succeeded
-        if (child == 0) { //currently child
-            execl("/bin/pwd", "pwd", 0, NULL);
-            perror("pwdCommand"); //if we reached here, there is an error so we must exit
-            exit(EXIT_FAILURE);
-        }
-        else {
-            wait(NULL);
-        }
-    }
-    return;
+    execl("/bin/pwd", "pwd", 0, NULL);
+    perror("pwdCommand"); //if we reached here, there is an error so we must exit
+    exit(EXIT_FAILURE);
 }
 
 void wcCommand(const char* inputName){
-    pid_t child = fork();
-    if (child < 0){
-        perror("Failed fork child");
-        exit(EXIT_FAILURE);
-    }
-
-    else { //fork child succeeded
-        if (child == 0) { //currently child
-            execl("/bin/wc", "wc", inputName, NULL);
-            perror("wcCommand"); //if we reached here, there is an error so we must exit
-            exit(EXIT_FAILURE);
-        }
-        else {
-            wait(NULL);
-        }
-    }
-    return;
+    execl("/bin/wc", "wc", inputName, NULL);
+    perror("wcCommand"); //if we reached here, there is an error so we must exit
+    exit(EXIT_FAILURE);
 }
--- mainShell.c
+++ mainShell.c
@@ -12,35 +12,47 @@ void welcome(void){
 run the appropriate command
 */
 void runCommand(int commandNum, char* args[]){
-    switch (commandNum) {
-        case 0:
-            lsCommand();
-            break;
-        case 1:
-            pwdCommand();
-            break;
-        case 2:
-            printf("wc call\n");
-            //add current pwd and inputName to get the file
-            //but what happens if user actually provides the absolute path?
-
-            char path[maxInput];
-            getcwd(path, maxInput);
-            // printf("%s\n", path);
-            // printf("%s\n", commandParse[1]);
-            
-            char name_with_extension[maxInput];
-            strcpy(name_with_extension, path); /* copy name into the new var */
-            strcat(name_with_extension, "/"); /* add the extension */
-            strcat(name_with_extension, args[1]);
-            printf("%s\n", name_with_extension);
-            wcCommand(name_with_extension);
-            
-            break;
-        default: //when -1 is called
-            printf("Command not recognized, please try again\n");
-            break;
+    pid_t child = fork();
+    if (child < 0){ //fork failed
+        perror("Failed fork child");
+        exit(EXIT_FAILURE);
     }
+    else { //fork succeeded
+        if (child == 0) { //currently child
+            switch (commandNum) {
+                case 0:
+                    lsCommand();
+                    break;
+                case 1:
+                    pwdCommand();
+                    break;
+                case 2:
+                    printf("wc call\n");
+                    //add current pwd and inputName to get the file
+                    //but what happens if user actually provides the absolute path?
+
+                    char path[maxInput];
+                    getcwd(path, maxInput);
+                    // printf("%s\n", path);
+                    // printf("%s\n", commandParse[1]);
+                    
+                    char name_with_extension[maxInput];
+                    strcpy(name_with_extension, path); /* copy name into the new var */
+                    strcat(name_with_extension, "/"); /* add the extension */
+                    strcat(name_with_extension, args[1]);
+                    printf("%s\n", name_with_extension);
+                    wcCommand(name_with_extension);
+                    
+                    break;
+                default: //when -1 is called
+                    printf("Command not recognized, please try again\n");
+                    break;
+            }
+        } //end of child
+        else { //parent
+            wait(NULL);
+        }
+    } //end of fork
 }
 
 int main(){
