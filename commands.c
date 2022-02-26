7b8b658ca764c93ddc5ac1b5209b0e847dae9839
Brian Kim <hk2473@nyu.edu>
2022-02-26 17:57:42 +0400

    change from execl to execlp to handle path

--- commands.c
+++ commands.c
@@ -1,19 +1,19 @@
 #include "functions.h"
 
 void lsCommand(){
-    execl("/bin/ls", "ls", 0, NULL);
+    execlp("ls", "ls", 0, NULL);
     perror("lsCommand"); //if we reached here, there is an error so we must exit
     exit(EXIT_FAILURE);
 }
 
 void pwdCommand(){
-    execl("/bin/pwd", "pwd", 0, NULL);
+    execlp("pwd", "pwd", 0, NULL);
     perror("pwdCommand"); //if we reached here, there is an error so we must exit
     exit(EXIT_FAILURE);
 }
 
 void wcCommand(const char* inputName){
-    execl("/usr/bin/wc", "wc", inputName, NULL);
+    execlp("wc", "wc", inputName, NULL);
     perror("wcCommand"); //if we reached here, there is an error so we must exit
     exit(EXIT_FAILURE);
 }
