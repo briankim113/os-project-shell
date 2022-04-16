094bbb68d6a294f52064b22c5d28b47d0d442d98
Brian Kim <hk2473@nyu.edu>
2022-04-16 14:39:44 +0400

    copy phase2 to phase3

---
+++ phase3/client.c
@@ -0,0 +1,110 @@
+#include "functions.h"
+
+/*
+prints welcome messages
+*/
+void welcome()
+{
+    printf("Welcome to the main shell!\n");
+    printf("If you wish to exit the shell, use 'exit'\n");
+}
+
+/*
+checks if the user input is all whitespace
+and also replaces the last character from '\n' to '\0'
+*/
+
+int isInputEmpty(char *str){
+    printf(">>> ");
+
+    char buf[maxInput];
+    fgets(buf, maxInput, stdin);
+    buf[strlen(buf)-1] = '\0';
+    
+    if (*buf == '\0') { 
+        return 1;
+    }
+    else {
+        char *tmp = buf;
+        while (*tmp != '\0') {
+            if (*tmp != ' ') {
+                strcpy(str, buf);
+                return 0;
+            }
+            tmp++;
+        }
+        return 1;
+    }
+}
+
+int main(){
+
+	//create a socket
+	int network_socket;
+	network_socket = socket(AF_INET , SOCK_STREAM, 0);
+
+	//check for fail error
+	if (network_socket == -1) {
+        printf("socket creation failed..\n");
+        exit(EXIT_FAILURE);
+    }
+    
+
+	//connect to another socket on the other side
+	
+	//specify an address for the socket we want to connect to
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+
+	//connect
+	int connection_status = 
+	connect(network_socket, 
+			(struct sockaddr *) &server_address,
+			sizeof(server_address));
+
+	//check for errors with the connection
+	if(connection_status == -1){
+		printf("There was an error making a connection to the remote socket \n\n");
+		exit(EXIT_FAILURE);
+	}
+
+    //********** shell begins here ****************
+	
+    welcome();
+    char inputString[maxInput];
+    char response[maxInput]; //with TCP & SOCK_STREAM, buffer size for recv doesn't seem to matter anyway - https://stackoverflow.com/questions/2862071/how-large-should-my-recv-buffer-be-when-calling-recv-in-the-socket-library
+    
+    while (1)
+    {
+        //get the user input and save it inside inputString
+        if (isInputEmpty(inputString))
+            continue; //if empty, continue asking for actual input
+
+        //if "exit", then stop the shell
+        if (strcmp(inputString, "exit") == 0)
+        {
+            printf("goodbye!\n");
+            break;
+        }
+
+        //send the input without any cleaning to the server directly, where it will be handled
+        if (send(network_socket, inputString, sizeof(inputString), 0) <= 0){
+            perror("send error ");
+        }
+
+        //receive response back from the server
+        if (recv(network_socket, response, sizeof(response), 0) <= 0){
+            perror("recv error ");
+        }
+
+        //print the response
+        printf("%s\n", response);
+    }
+
+	//close socket after we are done
+	close(network_socket);
+	return 0;
+}
---
+++ phase3/functions.h
@@ -0,0 +1,35 @@
+//libraries
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+//variables
+#define maxInput 4096
+#define cmdListLen 10
+#define maxCmds 5
+#define maxArgs 5
+#define PORT 9002 //or 8080 or any other unused port value
+
+//inputHandling.c
+void redirection(char*, int*, int*, char*);
+void inputDecode(char* [], size_t, char*, int*);
+int parseCommand(char* [], size_t, char*);
+
+//commands.c
+void lsCommand(char* const*);
+void pwdCommand(char* const*);
+void wcCommand(char* const*);
+void psCommand(char* const*);
+void topCommand(char* const*);
+void echoCommand(char* const*);
+void touchCommand(char* const*);
+void lessCommand(char* const*);
+void grepCommand(char* const*);
+void catCommand(char* const*);
